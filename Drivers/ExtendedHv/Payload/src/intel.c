adfb77973d9693344edc3c76d554f1184b9d2d0e
Anonymous <no@no.no>
2026-01-14 14:31:57 +0000

    Still ignoring AMD, Intel should be in a better position, now that I have gotten my PC to crash, now its time to make it not crash.

--- Drivers/ExtendedHv/Payload/src/intel.c
+++ Drivers/ExtendedHv/Payload/src/intel.c
@@ -1,4 +1,11 @@
+#include "intel.h"
+
 #include <stdint.h>
+
+typedef uint64_t (__attribute__((ms_abi)) *original_vmexit_handler_t)(context_t *context, uint32_t exit_reason, uint32_t exit_reason_full);
+
+// Import
+extern void serial_write(const char *string);
 
 // Public Globals
 __attribute__((section(".data.global")))
@@ -6,23 +13,33 @@ int64_t G_original_offset_from_hook = 0x0; // its value is set through a patch o
 
 // Public Functions
 __attribute__((section(".text.function")))
-uint64_t __attribute__((ms_abi)) hooked_vmexit_handler(/* TODO */); // this function is called through file offset and must come straight after the global
+uint64_t __attribute__((ms_abi)) hooked_vmexit_handler(context_t *context, uint32_t exit_reason, uint32_t exit_reason_full);
 
 // Private Globals
-// None
+static int g_vmexit_called = 0;
 
 // Private Functions
 // None
 
-
 // Implementation
 
+uint64_t __attribute__((ms_abi)) hooked_vmexit_handler(void *context, uint32_t exit_reason, uint32_t exit_reason_full) {
+  //
+  // Attempt to debug serial print on the first run through (serial printing could fail at such a mature state in the os)
+  // 
+  if (!g_vmexit_called) {
+    g_vmexit_called = 1;
+    serial_write("[+] Intel VM-Exit handler active\n");
+  }
 
-uint64_t __attribute__((ms_abi)) hooked_vmexit_handler(/* TODO */) {
 
-
-
-
-  return 0;  
+  //
+  // Call original handler
+  // 
+  original_vmexit_handler_t original = (original_vmexit_handler_t)(
+      (uint64_t)hooked_vmexit_handler + G_original_offset_from_hook
+  );
+  return original(context, exit_reason, exit_reason_full);
 }
+
 
